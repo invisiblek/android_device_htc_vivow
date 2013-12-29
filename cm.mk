2f9dccc4fc6386c4ca72d8d7d3e289d98e1f0cb1
ChillyBean <ivanarroyo86@gmail.com>
2013-12-29 15:30:51 -0500

    back to cm!
    Signed-off-by: ChillyBean <ivanarroyo86@gmail.com>

---
+++ cm.mk
@@ -0,0 +1,20 @@
+# Inherit AOSP device configuration for inc.
+$(call inherit-product, device/htc/vivow/full_vivow.mk)
+
+# Inherit some common Cyanogen stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+#
+# Setup device specific product configuration.
+#
+PRODUCT_NAME := cm_vivow
+PRODUCT_BRAND := verizon_wwe
+PRODUCT_DEVICE := vivow
+PRODUCT_MODEL := ADR6350
+PRODUCT_MANUFACTURER := HTC
+
+
+# USB
+#ADDITIONAL_DEFAULT_PROPERTIES += persist.sys.usb.config=mass_storage,adb
+#ADDITIONAL_DEFAULT_PROPERTIES += persist.sys.usb.config=mtp,adb
